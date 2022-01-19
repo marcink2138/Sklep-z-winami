48861b7956dc4b70ffc900a2f5d5baa25f13507b
“Marcin <marcin.karcz374@interia.pl>
2022-01-19 22:24:12 +0100

    logowanko rejestrowanko

--- API/login.php
+++ API/login.php
@@ -4,5 +4,12 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 
-$customerAPI = new CustomerAPI();
-$customerAPI->logInCustomer();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $customerAPI = new CustomerAPI();
+    $customerAPI->logInCustomer();
+} else {
+    http_response_code(404);
+    echo json_encode(array(
+        "message"=>"Post method required"
+    ));
+}
--- API/register.php
+++ API/register.php
@@ -4,5 +4,13 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 
-$customerAPI = new CustomerAPI();
-$customerAPI->registerCustomer();
+
+if ($_SERVER['REQUEST_METHOD']=='POST') {
+    $customerAPI = new CustomerAPI();
+    $customerAPI->registerCustomer();
+}else{
+    http_response_code(404);
+    echo json_encode(array(
+        "message"=>"Post method required"
+    ));
+}
