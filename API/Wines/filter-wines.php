4ae8ec7f4b44582c8ae4ecc8543d279c99ae6fc1
“Marcin <marcin.karcz374@interia.pl>
2022-02-12 12:58:39 +0100

    WITH FILTERING

---
+++ API/Wines/filter-wines.php
@@ -0,0 +1,17 @@
+<?php
+
+include_once '../config/errorConfig.php';
+include_once '../WinesManagement/WinesAPI.php';
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $winesAPI = new WinesAPI();
+    $winesAPI->filterWines();
+} else {
+    http_response_code(404);
+    echo json_encode(array(
+        "message" => "GET method required"
+    ));
+}
--- API/WinesManagement/WinesManagement.php
+++ API/WinesManagement/WinesManagement.php
@@ -44,9 +44,62 @@ class WinesManagement
             if ($stmt->execute())
                 return true;
             return false;
-        }catch (PDOException $e){
+        } catch (PDOException $e) {
             return false;
         }
+    }
+
+    public function filterWines()
+    {
+        $query = "SELECT * FROM wine WHERE 1";
+        $filters = $this->getFilters();
+        if (!empty($filters)) {
+            foreach ($filters as $filter) {
+                $query = $query . $filter;
+            }
+        }
+        $stmt = $this->conn->prepare($query);
+        if (isset($_GET['country'])) {
+            $stmt->bindParam(":country", $_GET['country']);
+            echo $_GET['country'];
+        }
+        if (isset($_GET['importerName'])) {
+            $stmt->bindParam(":importerName", $_GET['importerName']);
+        }
+        echo $query;
+        $stmt->execute();
+        return $stmt;
+    }
+
+    private function getFilters()
+    {
+        $filters = array();
+        if (isset($_GET['country'])) {
+            $filters[] = " AND country = :country";
+        }
+        if (isset($_GET['importerName'])) {
+            $filters[] = " AND id_importer = (SELECT id_importer FROM importer WHERE name = :name)";
+        }
+        if (isset($_GET['price'])) {
+            if ($_GET['price'] == "DESC") {
+                $filters[] = " ORDER BY price DESC";
+            } else {
+                $filters[] = " ORDER BY price ASC";
+            }
+        } elseif (isset($_GET['quantity'])) {
+            if ($_GET['quantity'] == "DESC") {
+                $filters[] = " ORDER BY quantity DESC";
+            } else {
+                $filters[] = " ORDER BY quantity ASC";
+            }
+        } elseif (isset($_GET['name'])) {
+            if ($_GET['name'] == "DESC") {
+                $filters[] = " ORDER BY name DESC";
+            } else {
+                $filters[] = " ORDER BY name ASC";
+            }
+        }
+        return $filters;
     }
 
 }
