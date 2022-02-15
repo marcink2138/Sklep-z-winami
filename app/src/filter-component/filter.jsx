062076725caed5606a6a6c6ec864695a55e7a7c8
“Marcin <marcin.karcz374@interia.pl>
2022-02-15 19:35:44 +0100

    filtrowanko

---
+++ app/src/filter-component/filter.jsx
@@ -0,0 +1,211 @@
+import React, { useState } from "react";
+import ProductList from "../product-component/productList";
+import './style.scss'
+export default class FilterComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            country: "",
+            name: "",
+            importerName: "",
+            price: "",
+            countryList: [],
+            importerNamesList: [],
+            isImporterListPresent: false,
+            isCountryListPresent: false,
+            filteredWines: [],
+            areFilteredWinesPresent: false
+        };
+        this.getWinesWithoutFilters();
+        this.getImporterList();
+        this.getCountryList();
+        this.handleCountryChange = this.handleCountryChange.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handlePriceChange = this.handlePriceChange.bind(this);
+        this.handleImporterChange = this.handleImporterChange.bind(this);
+        this.handleFilter = this.handleFilter.bind(this);
+    }
+
+    getImporterList() {
+        const data = {
+            jwt: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NDQ5MzM5NzAsIm5iZiI6MTY0NDkzMzk3MCwiZXhwIjoxNjQ0OTUxOTcwLCJpc3MiOiJodHRwczovL3M0MDIzNDAubGFiYWdoLnBsL0FQSSIsImRhdGEiOnsiaWQiOiI5IiwiYWNjVHlwZSI6IkN1c3RvbWVyIn19.NpsZezoNEP2s2anG0GG0FzIw27i2paSNIr8CapMKDtE"
+        }
+        fetch('https://s402340.labagh.pl/API/Importer/get-all-importers.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                this.setState({ isImporterListPresent: true })
+                this.setState({ importerNamesList: data.importers })
+            })
+            .catch((error) => {
+                console.error('Error', error);
+            });
+    }
+
+    getCountryList() {
+        const data = {
+            jwt: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NDQ5MzM5NzAsIm5iZiI6MTY0NDkzMzk3MCwiZXhwIjoxNjQ0OTUxOTcwLCJpc3MiOiJodHRwczovL3M0MDIzNDAubGFiYWdoLnBsL0FQSSIsImRhdGEiOnsiaWQiOiI5IiwiYWNjVHlwZSI6IkN1c3RvbWVyIn19.NpsZezoNEP2s2anG0GG0FzIw27i2paSNIr8CapMKDtE"
+        }
+        fetch('https://s402340.labagh.pl/API/Wines/countries.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                this.setState({ isCountryListPresent: true })
+                this.setState({ countryList: data.countries })
+            })
+            .catch((error) => {
+                console.error('Error', error);
+            });
+    }
+
+    getWinesWithoutFilters(){
+        this.setState({areFilteredWinesPresent:false})
+        const data = {
+            jwt: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NDQ5MzM5NzAsIm5iZiI6MTY0NDkzMzk3MCwiZXhwIjoxNjQ0OTUxOTcwLCJpc3MiOiJodHRwczovL3M0MDIzNDAubGFiYWdoLnBsL0FQSSIsImRhdGEiOnsiaWQiOiI5IiwiYWNjVHlwZSI6IkN1c3RvbWVyIn19.NpsZezoNEP2s2anG0GG0FzIw27i2paSNIr8CapMKDtE"
+        }
+        fetch(`https://s402340.labagh.pl/API/Wines/filter-wines.php`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                this.setState({ filtredWines: data.wines })
+                this.setState({areFilteredWinesPresent:true})
+            })
+            .catch((error) => {
+                console.error('Error', error);
+            });
+    }
+
+    handleFilter = (e) => {
+        this.setState({areFilteredWinesPresent:false})
+        let areSelectsEmpty = true;
+        let tempArr = []
+        let tempParamString
+        if (this.state.country != "") {
+            tempArr.push('country=' + this.state.country)
+            areSelectsEmpty = false
+        }
+
+        if (this.state.importerName != "") {
+            tempArr.push('importerName=' + this.state.importerName)
+            areSelectsEmpty = false
+        }
+
+        if (this.state.price != "") {
+            tempArr.push('price=' + this.state.price)
+            areSelectsEmpty = false
+        }
+
+        if (this.state.name != "") {
+            tempArr.push('name=' + this.state.name)
+            areSelectsEmpty = false
+        }
+        if (!areSelectsEmpty) {
+            tempParamString = '?'
+            tempArr.forEach(element => {
+                tempParamString += element + '&'
+            });
+            tempParamString = tempParamString.slice(0, -1)
+        } else
+            tempParamString = ""
+
+        const data = {
+            jwt: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NDQ5MzM5NzAsIm5iZiI6MTY0NDkzMzk3MCwiZXhwIjoxNjQ0OTUxOTcwLCJpc3MiOiJodHRwczovL3M0MDIzNDAubGFiYWdoLnBsL0FQSSIsImRhdGEiOnsiaWQiOiI5IiwiYWNjVHlwZSI6IkN1c3RvbWVyIn19.NpsZezoNEP2s2anG0GG0FzIw27i2paSNIr8CapMKDtE"
+        }
+        fetch(`https://s402340.labagh.pl/API/Wines/filter-wines.php${tempParamString}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                this.setState({ filtredWines: data.wines })
+                this.setState({areFilteredWinesPresent:true})
+            })
+            .catch((error) => {
+                console.error('Error', error);
+            });
+    }
+    handleCountryChange = (e) => {
+        this.setState({ country: e.target.value })
+    }
+    handleImporterChange = (e) => {
+        this.setState({ importerName: e.target.value })
+    }
+    handlePriceChange = (e) => {
+        this.setState({ price: e.target.value })
+    }
+    handleNameChange = (e) => {
+        this.setState({ name: e.target.value })
+    }
+    render() {
+        const countryList = this.state.countryList;
+        const importerList = this.state.importerNamesList;
+        return (
+            <div>
+                <div className="filters">
+                    {this.state.isCountryListPresent &&
+                        <div className="filter">
+                            <h1>Kraj</h1>
+                            <select id="countrySelect" onChange={this.handleCountryChange}>
+                                <option value=""></option>
+                                {countryList.map((country) => (
+                                    <option value={country.country}>{country.country}</option>
+                                ))}
+                            </select>
+                        </div>
+                    }
+                    {this.state.isImporterListPresent &&
+                        <div className="filter">
+                            <h1>Dostawca</h1>
+                            <select id="importerSelect" onChange={this.handleImporterChange} >
+                                <option value=""></option>
+                                {importerList.map((importer) => (
+                                    <option value={importer.name}>{importer.name}</option>
+                                ))}
+                            </select>
+                        </div>
+                    }
+                    <div className="filter">
+                        <h1>Po cenie</h1>
+                        <select id="importerSelect" onChange={this.handlePriceChange} >
+                            <option value=""></option>
+                            <option value='ASC'>Malejąco</option>
+                            <option value='DESC'>Rosnąco</option>
+                        </select>
+                    </div>
+                    <div className="filter">
+                        <h1>Po nazwie</h1>
+                        <select id="importerSelect" onChange={this.handleNameChange} >
+                            <option value=""></option>
+                            <option value='ASC'>Malejąco</option>
+                            <option value='DESC'>Rosnąco</option>
+                        </select>
+                    </div>
+                    <div>
+                        <button className="filterButton" onClick={this.handleFilter}>Filtruj</button>
+                    </div>
+                </div>
+                <div>{this.state.areFilteredWinesPresent &&
+                    <ProductList wineList = {this.state.filtredWines}/>}
+                </div>
+            </div>
+        )
+    }
+}
