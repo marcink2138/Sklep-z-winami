3f2e556407aa895334ab47ddac3e634a82e7cd08
Wiktor Tekiela <56120539+abcdrefg@users.noreply.github.com>
2022-02-15 19:17:27 +0100

    wio

--- app/src/product-component/productList.jsx
+++ app/src/product-component/productList.jsx
@@ -8,34 +8,34 @@ export default class ProductList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            wineList: [],
-            winesNotEmpty: false
+            wineList: this.props.wineList
+            //winesNotEmpty: false
         }
-        this.getWines();
+        //this.getWines();
         console.log("Dupa");
     }
 
-    getWines() {
-        let json
-        const data = {
-            jwt: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NDQ4NTk3MzEsIm5iZiI6MTY0NDg1OTczMSwiZXhwIjoxNjQ0ODc3NzMxLCJpc3MiOiJodHRwczovL3M0MDIzNDAubGFiYWdoLnBsL0FQSSIsImRhdGEiOnsiaWQiOiI5IiwiYWNjVHlwZSI6IkN1c3RvbWVyIn19.f4PRoLl7PsV8cmIvxv-l7I14-ZO0KLlDd4Zhyq1eb3M"
-        }
-        fetch('https://s402340.labagh.pl/API/Wines/filter-wines.php', {
-            method: 'POST',
-            body: JSON.stringify(data),
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Success:', data);
-            json = data.wines;
-            this.setState({wineList: json});
-            this.setState({winesNotEmpty: true});
-        })
-        .catch((error) => {
-            console.error('Error', error);
-        });
-        console.log('test1');
-    }
+    // getWines() {
+    //     let json
+    //     const data = {
+    //         jwt: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NDQ4NTk3MzEsIm5iZiI6MTY0NDg1OTczMSwiZXhwIjoxNjQ0ODc3NzMxLCJpc3MiOiJodHRwczovL3M0MDIzNDAubGFiYWdoLnBsL0FQSSIsImRhdGEiOnsiaWQiOiI5IiwiYWNjVHlwZSI6IkN1c3RvbWVyIn19.f4PRoLl7PsV8cmIvxv-l7I14-ZO0KLlDd4Zhyq1eb3M"
+    //     }
+    //     fetch('https://s402340.labagh.pl/API/Wines/filter-wines.php', {
+    //         method: 'POST',
+    //         body: JSON.stringify(data),
+    //     })
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log('Success:', data);
+    //         json = data.wines;
+    //         this.setState({wineList: json});
+    //         this.setState({winesNotEmpty: true});
+    //     })
+    //     .catch((error) => {
+    //         console.error('Error', error);
+    //     });
+    //     console.log('test1');
+    // }
 
 
     render() {
@@ -43,8 +43,7 @@ export default class ProductList extends React.Component {
         const isReady = this.state.winesNotEmpty;
         return(
             <div className="productList">
-                {isReady &&
-                 wineList.map((wine) => (
+                {wineList.map((wine) => (
                     <Product name = {wine.name} price = {wine.price} id = {wine.id}/>
                 ))}
             </div>
